4a677fdce03ba280c6d5c0f8147a0b73a8594936
CarterTheMan <cartermoseley20@gmail.com>
2024-01-02 01:37:38 -0600

    For rabbits and foxes, when they mate there is a wait and they only mate with specific chosen mate. They both walk faster to correspond with the speed they have. "

--- Assets/FoxScript.cs
+++ Assets/FoxScript.cs
@@ -12,10 +12,15 @@ public class FoxScript : MonoBehaviour
     private float hunger;
 
     // Hunting / Mating
-    private string preyType = "Rabbit";
+    private const string preyType = "Rabbit";
     private GameObject huntedPrey = null;
     private GameObject mate = null;
     private float matingHungerThreshold = 0.5f;
+
+    // Waiting
+    private bool waitBool = false;
+    private float waitTotalTime;
+    private float waitCounter;
 
     // Movement
     private int moveTime;
@@ -41,17 +46,26 @@ public class FoxScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Handle hunger
-        Hunger();
+        // If not in a waiting state, continue as normal
+        if (!waitBool) {
+            // Handle hunger
+            Hunger();
 
-        // If in sight, set the prey
-        huntedPrey = FindNextTarget(preyType);
+            // If in sight, set the prey
+            huntedPrey = FindNextTarget(preyType);
 
-        // If in sight, set a mate
-        mate = FindNextTarget(tag);
+            // If in sight, set a mate
+            mate = FindNextTarget(tag);
 
-        // Move towards prey or randomly
-        move();
+            // Move towards mate then prey then randomly
+            move();
+        } else {
+            waitCounter++;
+            if (waitCounter > waitTotalTime) {
+                waitCounter = 0;
+                waitBool = false;
+            }
+        }
     }   
 
     // Cost hunger, die if starve
@@ -66,6 +80,11 @@ public class FoxScript : MonoBehaviour
     GameObject FindNextTarget(string searchTag) {
         GameObject finalTarget = null;
         float closestFinalTarget = float.MaxValue;
+
+        // Skip this if searching for mate and too hungry
+        if (searchTag == tag && hunger <= (maxHunger * matingHungerThreshold)) {
+            return finalTarget;
+        }
         
         // Find target in the right range
         GameObject[] targets = GameObject.FindGameObjectsWithTag(searchTag);
@@ -86,7 +105,7 @@ public class FoxScript : MonoBehaviour
                 // Is closest prey
                 switch (searchTag) {
                     // Prey
-                    case var preyType when preyType == preyType:
+                    case preyType:
                         if (closer) {
                             finalTarget = target;
                             closestFinalTarget = distancetoPrey;
@@ -94,7 +113,7 @@ public class FoxScript : MonoBehaviour
                         break;
                     
                     // Mate
-                    case var tag when tag == tag:
+                    case "Fox":
                         bool canSelfMate = hunger > (maxHunger * matingHungerThreshold);
                         bool canTargetMate = target.GetComponent<FoxScript>().hunger > (target.GetComponent<FoxScript>().maxHunger * matingHungerThreshold);
                         if (canSelfMate && canTargetMate && closer) {
@@ -118,7 +137,7 @@ public class FoxScript : MonoBehaviour
         // How fast to move forward
         var step = speed * Time.deltaTime;
 
-        // Go to mate, else hungt prey, else passive move
+        // Go to mate, else hunt prey, else passive move
         if (mate != null) {
             GetComponent<Renderer>().material.color = mateColor;
 
@@ -139,46 +158,63 @@ public class FoxScript : MonoBehaviour
 
             // Once time expires, reset
             if (moveCounter >= moveTime) {
-                walk = !walk;
-                moveCounter = 0;
-
-                // If it should walk, go for 1-10 seconds. Else turn for 3-5 seconds. 
-                if (walk) {
-                    moveTime = Random.Range(frameRate, 10 * frameRate);
-                } else {
-                    moveTime = Random.Range(3 * frameRate, 5 * frameRate);
-                    turnRate = new Vector3(0, Random.Range(-60.0f, 60.0f), 0);
-                }
+                swapWalkAndTurn();
             }
 
             moveCounter++;
             // If walking forward, else turning
             if (walk) {
-                transform.Translate(Vector3.forward * Time.deltaTime);
+                transform.Translate(Vector3.forward * step);
             } else {
                 transform.Rotate(turnRate * Time.deltaTime);
             }
         }
     }
 
-    // When collide with prey, delete the prey
-    private IEnumerator OnCollisionEnter(Collision other) {
-        // If collides with mate
-        if (other.gameObject.tag == tag) {
-            // TODO: Get the wait to work
-            // Subtract hunger, erase mate and wait
-            hunger -= (int)(maxHunger * matingHungerThreshold);
-            mate = null;
-            yield return new WaitForSeconds(2);
+    // If walking, then turn. If turning, then walk
+    void swapWalkAndTurn() {
+        walk = !walk;
+        moveCounter = 0;
 
-            // TODO: add new baby fox
-        
+        // If it should walk, go for 1-10 seconds. Else turn for 3-5 seconds. 
+        if (walk) {
+            moveTime = Random.Range(frameRate, 10 * frameRate);
+        } else {
+            int minTime = 3;
+            int maxTime = 5;
+            moveTime = Random.Range(minTime * frameRate, maxTime * frameRate);
+            turnRate = new Vector3(0, Random.Range(-360.0f / maxTime, 360.0f / maxTime), 0);
+        }
+    }
+
+    // When collide with prey, delete the prey
+    void OnCollisionEnter(Collision other) {
+        // If collides with mate
+        if (other.gameObject.tag == tag && (mate == other.gameObject || other.gameObject.GetComponent<FoxScript>().mate == this.gameObject)) {
+            // Subtract hunger and look at each other
+            hunger -= (int)(maxHunger * matingHungerThreshold);
+            Vector3 targetVector = new Vector3(other.transform.position.x, 0.5f, other.transform.position.z);
+            transform.LookAt(targetVector);
+
+            // Wait and then make baby fox
+            startWait(2);
+            // TODO: add new baby fox and decide if the wait it needed
+
+            // Look away from each other and continue moving
+            transform.RotateAround(transform.position, transform.up, 180f);
+            swapWalkAndTurn();
 
         // If collides with prey
         } else if (other.gameObject.tag == preyType) {
             hunger += (int)(other.gameObject.transform.localScale.x * 1000);
             Destroy(other.gameObject);
         }
+    }
+
+    // Time in seconds to wait
+    void startWait(int time) {
+        waitBool = true;
+        waitTotalTime = time * frameRate;
     }
 }
 
--- Assets/RabbitScript.cs
+++ Assets/RabbitScript.cs
@@ -8,14 +8,19 @@ public class RabbitScript : MonoBehaviour
     public float FOV;
     public float viewDistance;
     public float speed;
-    private float maxHunger = 1500;
+    private float maxHunger = 4500;
     private float hunger;
     
     // Hunting / Mating
-    private string preyType = "Bush";
+    private const string preyType = "Bush";
     private GameObject huntedPrey = null;
     private GameObject mate = null;
     private float matingHungerThreshold = 0.5f;
+
+    // Waiting
+    private bool waitBool = false;
+    private float waitTotalTime;
+    private float waitCounter;
 
     // Movement
     private int frameRate;
@@ -41,17 +46,27 @@ public class RabbitScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Handle hunger
-        Hunger();
+        if (!waitBool) {
+            // Handle hunger
+            Hunger();
+            
+            // If in sight, set the prey
+            huntedPrey = FindNextTarget(preyType);
 
-        // If in sight, set the prey
-        huntedPrey = FindNextTarget(preyType);
+            // If in sight, set a mate
+            mate = FindNextTarget(tag);
 
-        // If in sight, set a mate
-        mate = FindNextTarget(tag);
+            // Move towards mate then prey then randomly
+            Move();
+        } else {
+            waitCounter++;
+            if (waitCounter > waitTotalTime) {
+                waitCounter = 0;
+                waitBool = false;
+            }
+        }
 
-        // Move towards prey or randomly
-        Move();
+        
     }   
 
     // Cost hunger, die if starve
@@ -66,6 +81,11 @@ public class RabbitScript : MonoBehaviour
     GameObject FindNextTarget(string searchTag) {
         GameObject finalTarget = null;
         float closestFinalTarget = float.MaxValue;
+
+        // Skip this if searching for mate and too hungry
+        if (searchTag == tag && hunger <= (maxHunger * matingHungerThreshold)) {
+            return finalTarget;
+        }
         
         // Find target in the right range
         GameObject[] targets = GameObject.FindGameObjectsWithTag(searchTag);
@@ -86,7 +106,7 @@ public class RabbitScript : MonoBehaviour
                 // Is closest prey
                 switch (searchTag) {
                     // Prey
-                    case var preyType when preyType == preyType:
+                    case preyType:
                         if (closer) {
                             finalTarget = target;
                             closestFinalTarget = distancetoPrey;
@@ -94,7 +114,7 @@ public class RabbitScript : MonoBehaviour
                         break;
                     
                     // Mate
-                    case var tag when tag == tag:
+                    case "Rabbit":
                         bool canSelfMate = hunger > (maxHunger * matingHungerThreshold);
                         bool canTargetMate = target.GetComponent<RabbitScript>().hunger > (target.GetComponent<RabbitScript>().maxHunger * matingHungerThreshold);
                         if (canSelfMate && canTargetMate && closer) {
@@ -118,7 +138,7 @@ public class RabbitScript : MonoBehaviour
         // How fast to move forward
         var step = speed * Time.deltaTime;
 
-        // Go to mate, else hungt prey, else passive move
+        // Go to mate, else hunt prey, else passive move
         if (mate != null) {
             GetComponent<Renderer>().material.color = mateColor;
 
@@ -138,48 +158,65 @@ public class RabbitScript : MonoBehaviour
         } else {
             GetComponent<Renderer>().material.color = passiveColor;
 
-            // Once time expires, reset
+            // Once time expires, change walking and turning
             if (moveCounter >= moveTime) {
-                walk = !walk;
-                moveCounter = 0;
-
-                // If it should walk, go for 1-10 seconds. Else turn for 3-5 seconds. 
-                if (walk) {
-                    moveTime = Random.Range(frameRate, 10 * frameRate);
-                } else {
-                    moveTime = Random.Range(3 * frameRate, 5 * frameRate);
-                    turnRate = new Vector3(0, Random.Range(-60.0f, 60.0f), 0);
-                }
+                swapWalkAndTurn();
             }
 
             moveCounter++;
             // If walking forward, else turning
             if (walk) {
-                transform.Translate(Vector3.forward * Time.deltaTime);
+                transform.Translate(Vector3.forward * step);
             } else {
                 transform.Rotate(turnRate * Time.deltaTime);
             }
         }
     }
 
-    // When collide with prey, delete the prey
-    private IEnumerator OnCollisionEnter(Collision other) {
-        // If collides with mate
-        if (other.gameObject.tag == tag) {
-            // TODO: Get the wait to work
-            // Subtract hunger, erase mate and wait
-            hunger -= (int)(maxHunger * matingHungerThreshold);
-            mate = null;
-            yield return new WaitForSeconds(1);
+    // If walking, then turn. If turning, then walk
+    void swapWalkAndTurn() {
+        walk = !walk;
+        moveCounter = 0;
 
-            // TODO: add new baby rabbit
-        
+        // If it should walk, go for 1-10 seconds. Else turn for 3-5 seconds. 
+        if (walk) {
+            moveTime = Random.Range(frameRate, 10 * frameRate);
+        } else {
+            int minTime = 3;
+            int maxTime = 5;
+            moveTime = Random.Range(minTime * frameRate, maxTime * frameRate);
+            turnRate = new Vector3(0, Random.Range(-360.0f / maxTime, 360.0f / maxTime), 0);
+        }
+    }
+
+    // When collide with prey, delete the prey
+    void OnCollisionEnter(Collision other) {
+        // If collides with mate
+        if (other.gameObject.tag == tag && (mate == other.gameObject || other.gameObject.GetComponent<RabbitScript>().mate == this.gameObject)) {
+            // Subtract hunger and look at each other
+            hunger -= (int)(maxHunger * matingHungerThreshold);
+            Vector3 targetVector = new Vector3(other.transform.position.x, 0.5f, other.transform.position.z);
+            transform.LookAt(targetVector);
+
+            // Wait and then make baby rabbit
+            startWait(2);
+            // TODO: add new baby rabbit and decide if the wait it needed
+
+            // Look away from each other and continue moving
+            transform.RotateAround(transform.position, transform.up, 180f);
+            swapWalkAndTurn();
         
         // If collides with prey
         } else if (other.gameObject.tag == preyType) {
             hunger += (int)(other.gameObject.transform.localScale.x * 1000);
             Destroy(other.gameObject);
         }
+    }
+
+    // Time in seconds to wait
+    void startWait(int time) {
+        waitBool = true;
+        waitTotalTime = time * frameRate;
     }
 }
 
